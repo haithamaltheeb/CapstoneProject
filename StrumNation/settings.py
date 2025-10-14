f1a74de20718b16ec2dab4b4325ab8dd0b39a9e8
Haitham AlTheeb <haitham.deeb8@gmail.com>
2025-10-15 02:49:24 +0300

    orm_reaching

--- StrumNation/settings.py
+++ StrumNation/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'StrumNation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'strumnation',
+        'USER': 'postgres',
+        'PASSWORD':'0000',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
